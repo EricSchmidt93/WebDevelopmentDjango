0b68f4ab7a68c7bf73a70ad1c421bb06fb551f6c
AMajesticSeaFlapFlap <alex.tso.ger@gmail.com>
2017-03-20 11:21:46 +0100

    Started adding in some bootstrap

--- WebDevelopmentDjango/settings.py
+++ WebDevelopmentDjango/settings.py
@@ -114,3 +114,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = ''
+
+STATICFILES_FINDERS = ( 
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
