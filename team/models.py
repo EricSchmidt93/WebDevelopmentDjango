bfc3f9f4f4edb245ef1926c2ce1f03d5a67a1e83
AMajesticSeaFlapFlap <alex.tso.ger@gmail.com>
2017-04-12 08:51:36 +0200

    purged models.py

--- team/models.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-#  -*- coding: UTF-8 -*-
-# vim: set fileencoding=UTF-8 :
-
-from django.db import models
-
-class Team(models.Model):
-    name = models.CharField(max_length=30)
-    topic = models.CharField(max_length=30)
